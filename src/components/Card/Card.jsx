687b0947e5577648b62ce76da0a535fd0efd33e2
Magzhan <sunmorring@gmail.com>
2022-02-10 16:54:32 +0600

    edit card and header component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { MdFavoriteBorder } from 'react-icons/md';
 
 import style from './Card.module.scss';
 
 function Card({ name, imageUrl, price }) {
-  console.log(name);
+  const [isShow, setIsShow] = React.useState(false);
   return (
-    <div className={style.card}>
-      <img width={248} height={310} src={imageUrl} alt="img" />
-      <div className={style.name}>{name}</div>
-      <div className={style.price}>{price} &#8376;</div>
+    <div
+      className={style.wrapper}
+      onMouseEnter={() => setIsShow(true)}
+      onMouseLeave={() => setIsShow(false)}>
+      <div className={style.card}>
+        <img width={248} height={310} src={imageUrl} alt="img" />
+        <div className={style.name}>{name}</div>
+        <div className={style.price}>{price} &#8376;</div>
+        {isShow && (
+          <>
+            <div className={style.toCart}>В КОРЗИНУ</div>
+            <div className={style.iconFavorite}>
+              <MdFavoriteBorder size={26} />
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
